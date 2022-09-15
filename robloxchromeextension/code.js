eb82fd09fa584c3ef038261de374dd361109700f
Trent <99286664+TrentTrentTrent@users.noreply.github.com>
2022-09-14 21:10:37 -0400

    Create code.js

---
+++ robloxchromeextension/code.js
@@ -0,0 +1,11 @@
+window.addEventListener("load", function () {
+    if (document.querySelector("span.tooltip-pastnames") !== null){ 
+        var prevnames = document.querySelector("span.tooltip-pastnames").getAttribute("data-original-title").split(", ");
+        console.log('previous usernames: '+ prevnames.length + ' robux:' + prevnames.length + '000')
+        var usernameusernames = 'Username'
+        if (prevnames.length > 1){
+            usernameusernames = 'Usernames'
+        }
+        document.querySelector("span.text-pastname").innerHTML = '<span style="color: #808080;">'+ prevnames.length +' Previous '+ usernameusernames +'</span> <span style="color: #666699;">|</span> <span style="color: #339966;">R$ '+ prevnames.length +'000</span>'
+}
+  });
