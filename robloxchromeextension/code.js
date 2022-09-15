68267c7af52b2fbd3d0a5c13cb28a46f4a5196b3
Trent <99286664+TrentTrentTrent@users.noreply.github.com>
2022-09-14 21:36:12 -0400

    Delete robloxchromeextension directory

--- robloxchromeextension/code.js
+++
@@ -1,8 +0,0 @@
-    if (document.querySelector("span.tooltip-pastnames") !== null){ 
-        var prevnames = document.querySelector("span.tooltip-pastnames").getAttribute("data-original-title").split(", ");
-        var usernameusernames = 'Username'
-        if (prevnames.length > 1){
-            usernameusernames = 'Usernames'
-        }
-        document.querySelector("span.text-pastname").innerHTML = '<span style="color: #808080;">'+ prevnames.length +' Previous '+ usernameusernames +'</span> <span style="color: #666699;">|</span> <span style="color: #339966;">R$ '+ prevnames.length +'000</span>'
-}
