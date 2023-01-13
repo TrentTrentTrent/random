cf425aa71d175baaa9236737d23acebd140e80bc
Trent <99286664+TrentTrentTrent@users.noreply.github.com>
2023-01-13 17:28:06 -0500

    Create infinitechicken.lua

---
+++ kingofthehill/infinitechicken.lua
@@ -0,0 +1,2 @@
+game:GetService("ReplicatedStorage").Events.purchaseWeapon:InvokeServer("Ice Sword",-math.huge)
+game:GetService('Players').LocalPlayer.Backpack:WaitForChild('Ice Sword',5):Destroy()
